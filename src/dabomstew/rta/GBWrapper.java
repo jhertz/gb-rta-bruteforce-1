4fb248d4d1e6065c54cdf3ca33a84eeb28375833
jhertz <hectohertz@gmail.com>
2017-02-05 01:52:42 -0500

    merge commit

--- src/dabomstew/rta/GBWrapper.java
+++ src/dabomstew/rta/GBWrapper.java
@@ -5,18 +5,17 @@ import java.nio.ByteBuffer;
 import mrwint.gbtasgen.Gb;
 
 public class GBWrapper {
-	
+
 	private GBMemory mem;
 	private Gb gb;
 
-	public static final boolean STALL = true;
 	public GBWrapper(Gb gb, GBMemory mem) {
 		this.mem = mem;
 		this.gb = gb;
 	}
-	
+
 	public int advanceToAddress(int... addresses) {
-	    //System.out.println("advancing to "+printAddressList(addresses));
+		//System.out.println("advancing to "+printAddressList(addresses));
 		int result = 0;
 		//int runs = 0;
 		while(result == 0) {
@@ -27,62 +26,53 @@ public class GBWrapper {
 				result = gb.step(0, addresses);
 			}
 			//runs++;
-
-			if(STALL) {
-				try {
-					Thread.sleep(5);
-				} catch (InterruptedException e) {
-				}
+			try {
+				Thread.sleep(5);
+			} catch (InterruptedException e) {
 			}
 		}
 		mem.setStale();
 		//System.out.printf("returned after %d runs with result %04X\n", runs, result);
 		return result;
 	}
-	
+
 	public int advanceWithJoypadToAddress(int joypad, int... addresses) {
-        int result = 0;
-        while(result == 0) {
-            if(addresses.length == 0) {
-                result = gb.step(joypad);
-            }
-            else {
-                result = gb.step(joypad, addresses);
-            }
-            if(STALL) {
-				try {
-					Thread.sleep(5);
-				} catch (InterruptedException e) {
-				}
+		int result = 0;
+		while(result == 0) {
+			if(addresses.length == 0) {
+				result = gb.step(joypad);
 			}
-        }
-        mem.setStale();
-        return result;
-    }
-	
-	public void testSpeed() {
-	    long start = System.currentTimeMillis();
-	    for(int i=0;i<1000000;i++) {
-	        gb.step(0);
-	    }
-	    System.out.println("executed 1000000 frames in "+(System.currentTimeMillis()-start)+"ms");
+			else {
+				result = gb.step(joypad, addresses);
+			}
+            try {
+				Thread.sleep(5);
+            } catch (InterruptedException e) {
+            }
+		}
+		mem.setStale();
+		return result;
 	}
-	
+
+	public void testSpeed() {
+		long start = System.currentTimeMillis();
+		for(int i=0;i<1000000;i++) {
+			gb.step(0);
+		}
+		System.out.println("executed 1000000 frames in "+(System.currentTimeMillis()-start)+"ms");
+	}
+
 	public String printAddressList(int[] addresses) {
-	    StringBuilder sb = new StringBuilder("[");
-	    for(int i=0;i<addresses.length;i++) {
-	        sb.append(String.format("%X, ", addresses[i]));
-	    }
-	    sb.append("]");
-	    return sb.toString();
+		StringBuilder sb = new StringBuilder("[");
+		for(int i=0;i<addresses.length;i++) {
+			sb.append(String.format("%X, ", addresses[i]));
+		}
+		sb.append("]");
+		return sb.toString();
 	}
 
 	public void injectRBInput(int input) {
 		writeMemory(0xFFF8, input);
-	}
-
-	public void injectCrysInput(int input) {
-		writeMemory(0xFFA4, input);
 	}
 
 	public void writeMemory(int address, int value) {
@@ -94,15 +84,15 @@ public class GBWrapper {
 		gb.step(0);
 		mem.setStale();
 	}
-	
+
 	public void advanceFrame(int inputs) {
-        gb.step(inputs);
-        mem.setStale();
-    }
-	
+		gb.step(inputs);
+		mem.setStale();
+	}
+
 	public void loadState(ByteBuffer state) {
-	    gb.loadState(state);
-	    mem.setStale();
+		gb.loadState(state);
+		mem.setStale();
 	}
 
 }
